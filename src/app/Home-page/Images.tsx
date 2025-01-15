5b751426fd472f0ca65961f1e9a5485a4839b178
Sadiq-Memon <164412475+Sadiq-Memon@users.noreply.github.com>
2025-01-15 18:42:20 +0500

    Update Images.tsx

--- src/app/Home-page/Images.tsx
+++ src/app/Home-page/Images.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import Image from 'next/image'
-import Image, { StaticImageData } from 'next/image'
+import React from 'react';
+import Image, { StaticImageData } from 'next/image';
 
-
-interface ImageProps {
+interface ImagesProps { // Renamed to avoid potential conflicts
   src: string | StaticImageData;
   alt: string;
   Heading: string;
@@ -11,20 +9,20 @@ interface ImageProps {
   width?: number;
 }
 
-const Images : React.FC<ImageProps> =({src,alt,Heading,price,width=305})=> {
+const Images: React.FC<ImagesProps> = ({ src, alt, Heading, price, width = 305 }) => {
   return (
-    <main className='h-[462px] gap-[24px]'>
+    <main className="h-[462px] gap-[24px]">
       <div>
-            <Image src={src} alt={alt} className='h-[375px]' width={width} />
+        <Image src={src} alt={alt} className="h-[375px]" width={width} />
       </div>
-      <h4 className='font-normal text-[20px] leading-[28px] text-[#2A254B] pt-5'>
-      {Heading} 
+      <h4 className="font-normal text-[20px] leading-[28px] text-[#2A254B] pt-5">
+        {Heading}
       </h4>
-      <h5 className='font-normal text-[18px] leading-[27px] text-[#2A254B] pt-2'>
-      {price}
+      <h5 className="font-normal text-[18px] leading-[27px] text-[#2A254B] pt-2">
+        {price}
       </h5>
     </main>
-  )
-}
+  );
+};
 
-export default Images 
+export default Images;
